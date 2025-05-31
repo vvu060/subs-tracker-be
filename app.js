09b462e9eccd12847403a5058ccf015c2e98d4a5
Vishal Urankar <vishalurankar@Vishals-Mac-mini.local>
2025-05-31 16:21:40 +0530

    setup routes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+
 const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.checkout('/', (req, res) => {
+  res.send('Welcom to Subscription Tracker API');
+});
 
 app.listen(PORT, () => {
   console.log(
