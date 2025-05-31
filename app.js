80fdca96144b742a606d44a3909323f65b11732a
Vishal Urankar <vishalurankar@Vishals-Mac-mini.local>
2025-05-31 19:30:56 +0530

    setup error middleware

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { PORT } from './config/env.js';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabse from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+// Middlewares
+app.use(express.json()); // Middleware for handling JSON data
+app.use(express.urlencoded({ extended: false })); // Middleware for handling html form data
+app.use(cookieParser()); // Middleware for handling cookies
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+// Middleware for handling errors
+app.use(errorMiddleware);
 
 app.checkout('/', (req, res) => {
   res.send('Welcom to Subscription Tracker API');
