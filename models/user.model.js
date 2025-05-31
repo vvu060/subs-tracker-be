358e34213feafd2eface3d55180edbc2d9cfe565
Vishal Urankar <vishalurankar@Vishals-Mac-mini.local>
2025-05-31 17:13:46 +0530

    setup user & subscription models

---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'User name is required'],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minLength: 2,
+      maxLength: 255,
+      match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+    },
+    password: {
+      type: String,
+      required: [true, 'User Password is required'],
+      minLength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
