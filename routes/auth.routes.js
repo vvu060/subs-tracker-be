82ce2eb2e1db3251146e5e36b088ee2cd6ec6294
Vishal Urankar <vishalurankar@Vishals-Mac-mini.local>
2025-06-01 07:52:27 +0530

    setup auth controllers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,23 +1,12 @@
 import { Router } from 'express';
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => {
-  res.send({
-    title: 'Sign up',
-  });
-});
+authRouter.post('/sign-up', signUp);
 
-authRouter.post('/sign-in', (req, res) => {
-  res.send({
-    title: 'Sign in',
-  });
-});
+authRouter.post('/sign-in', signIn);
 
-authRouter.post('/sign-out', (req, res) => {
-  res.send({
-    title: 'Sign out',
-  });
-});
+authRouter.post('/sign-out', signOut);
 
 export default authRouter;
